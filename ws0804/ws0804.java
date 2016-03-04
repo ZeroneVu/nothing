f3fa07af665aa60a37fe285918156197b2911cec
Van Vu <van.vutrung@harveynash.vn>
2016-03-04 15:14:17 +0000

    master changed 2nd time

--- ws0804/ws0804.java
+++ ws0804/ws0804.java
@@ -5,6 +5,7 @@ public class ws0804 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Just simple comment to make sure that things had changed
+		// this is the 3rd line to change
 	}
 
 }
